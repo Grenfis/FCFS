be4225f398089612b8f38e8b02308dc5284ca372
Viktor <viktor.kubly@gmail.com>
2017-06-05 19:32:15 +1100

    Изменения, которые будут включены в коммит: 	изменено:      fcfs_opts.c
    Теперь при создании файла перед созданием новой записи в директории ищется
    первая свободная

--- fcfs/fcfs_opts.c
+++ fcfs/fcfs_opts.c
@@ -167,10 +167,25 @@ fcfs_mkdir(const char *path, mode_t mode) {
     int dirs_len = 0;
     int pfid = fcfs_get_pfid(path);
     fcfs_dir_entry_t *dirs = fcfs_read_directory(args, pfid, &dirs_len);
+    fcfs_dir_entry_t *tmp = NULL;
 
-    fcfs_dir_entry_t *tmp = calloc(1, sizeof(fcfs_dir_entry_t) * (dirs_len + 1));
-    memcpy(tmp, dirs, sizeof(fcfs_dir_entry_t) * dirs_len);
-    free(dirs);
+    int new_pos = -1;
+    for(size_t i = 0; i < dirs_len; ++i) {
+        if(strlen(dirs[i].name) == 0) {
+            new_pos = i;
+            break;
+        }
+    }
+
+    if(new_pos != -1) {
+        tmp = dirs;
+    }else{
+        dirs_len += 1;
+        tmp = calloc(1, sizeof(fcfs_dir_entry_t) * dirs_len);
+        memcpy(tmp, dirs, sizeof(fcfs_dir_entry_t) * (dirs_len - 1));
+        free(dirs);
+        new_pos = dirs_len - 1;
+    }
 
     int new_fid = fcfs_get_free_fid(args);
     if(new_fid <= 0) {
@@ -178,22 +193,18 @@ fcfs_mkdir(const char *path, mode_t mode) {
     }
 
     fcfs_alloc(args, new_fid);
-    //fcfs_init_dir(args, new_fid);
-    tmp[dirs_len].file_id       = new_fid; //set free file id
-    tmp[dirs_len].mode          = 0040000 | mode;
-    tmp[dirs_len].create_date   = time(NULL);
-    tmp[dirs_len].access_date   = time(NULL);
-    tmp[dirs_len].change_date   = time(NULL);
+    fcfs_init_dir(args, new_fid);
+    tmp[new_pos].file_id       = new_fid; //set free file id
+    tmp[new_pos].mode          = 0040000 | mode;
+    tmp[new_pos].create_date   = time(NULL);
+    tmp[new_pos].access_date   = time(NULL);
+    tmp[new_pos].change_date   = time(NULL);
 
     int p_len = get_parrent_path(path);
     p_len += 1;
-    strcpy(tmp[dirs_len].name, path + p_len);
+    strcpy(tmp[new_pos].name, path + p_len);
 
-    DEBUG("old dir len = %d", dirs_len);
-    DEBUG("new dir len = %d", dirs_len + 1);
-    DEBUG("name        = %s", path + p_len);
-
-    fcfs_write_directory(args, pfid, tmp, dirs_len + 1);
+    fcfs_write_directory(args, pfid, tmp, dirs_len);
 
     return 0;
 }
@@ -206,7 +217,7 @@ fcfs_rmdir(const char *path) {
     int fid = fcfs_get_fid(path);
     fcfs_remove_file(args, fid);
     fcfs_remove_from_dir(args, fcfs_get_pfid(path), fid);
-    
+
     return 0;
 }
 
